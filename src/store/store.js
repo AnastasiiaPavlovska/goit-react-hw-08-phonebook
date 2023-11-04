6cd428c8a50c229ad46d6c9464e3ffd80a045d0c
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-11-04 22:05:53 +0100

    add features

---
+++ src/store/store.js
@@ -0,0 +1,49 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import contactsReducer from "../features/contact/contactsSlice";
+import filterSlice from "features/filter/filterSlice";
+import authSlice from '../features/auth/auth-slice'
+
+
+
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+
+
+
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authSlice),
+    contacts: contactsReducer,
+    filter: filterSlice,
+  },
+  middleware (getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+  devTools: process.env.NODE_ENV === 'development',
+
+});
+
+
+export const persistor = persistStore(store);
+
